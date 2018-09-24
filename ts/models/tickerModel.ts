53c88079a4e7b805ef9d50963004672bb0014dd1
Tatenda Chawanzwa <info@tatendachawanzwa.com>
2018-09-24 15:35:06 +0100

    Corrected query params to brief

--- ts/models/tickerModel.ts
+++ ts/models/tickerModel.ts
@@ -1,33 +1,60 @@
-import * as merge from "deepmerge";
 import fetch from "node-fetch";
+const sort = require("fast-sort");
 
 const BASE_URL: string = "https://api.coinmarketcap.com/v2/ticker/";
 
 interface TickerQuery {
   start: number;
-  limit: number;
-  sort: "id" | "rank" | "volume_24h" | "percent_change_24h";
-  structure: "dictionary" | "array";
-  convert?: "AUD" | "BRL" | "CAD" | "CHF" | "CLP" | "CNY" | "CZK" | "DKK" | "EUR" | "GBP" | "HKD" | "HUF" | "IDR" | "ILS" | "INR" | "JPY" | "KRW" | "MXN" | "MYR" | "NOK" | "NZD" | "PHP" | "PKR" | "PLN" | "RUB" | "SEK" | "SGD" | "THB" | "TRY" | "TWD" | "ZAR" | "BTC" | "ETH" | "XRP" | "LTC" | "BCH";
+  elements: number;
+  sortBy: "marketCap" | "currentPriceUSD" | "volumeChangeInLast24hUSD" | "priceChangeLast24hUSD";
+  sortAsc: boolean;
+  filter?: string;
 }
 const DEFAULT_QUERY: TickerQuery = {
-  start: 1,
-  limit: 100,
-  sort: "rank",
-  structure: "dictionary",
+  start: 0,
+  elements: 100,
+  sortBy: "marketCap",
+  sortAsc: false,
 };
 
 async function tickerModel(query: TickerQuery = DEFAULT_QUERY): Promise<any> {
-  const updatedQuery: TickerQuery = merge(DEFAULT_QUERY, query);
-  const url: string = constructUrl(updatedQuery);
-  const response = await fetch(url);
-  const data = await response.json();
+  const coins = await fetchCoins();
+  const updatedQuery: TickerQuery = {...DEFAULT_QUERY, ...query};
+
+  const { start, elements, sortBy, sortAsc, filter } = updatedQuery;
+  const sort = sortAsc ? "asc" : "desc";
+  const sorted: any[] = sortCoins(coins, sortBy, sort);
+  let data: any[] = sorted.slice(start, elements);
+
+  if (filter) {
+    const regex = RegExp(filter);
+    data = data.filter(coin => regex.test(coin.name) || regex.test(coin.symbol));
+  }
+
   return data;
 }
 
-function constructUrl(query: TickerQuery): string {
-  let urlizedQuery: string = Object.keys(query).reduce((acc, key) => `${acc}&${key}=${query[key]}`, "");
-  return `${BASE_URL}?${urlizedQuery}`;
+async function fetchCoins(): Promise<any> {
+  const promises = [];
+  for (let i = 0; i < 1000; i += 100) {
+    const url = `${BASE_URL}?start=${i}&limit=100&structure=array`;
+    promises.push(fetch(url).then(data => data.json()));
+  }
+  const responses = await Promise.all(promises);
+  return responses.reduce((acc, resp) => acc.concat(resp.data), []);
+}
+
+const sortCoins = (coins, sortBy: "marketCap" | "currentPriceUSD" | "volumeChangeInLast24hUSD" | "priceChangeLast24hUSD", sortAsc: "asc" | "desc"): any[] => {
+  switch (sortBy) {
+    case "currentPriceUSD":
+      return sort(coins)[sortAsc](c => c.quotes.USD.price);
+    case "volumeChangeInLast24hUSD":
+      return sort(coins)[sortAsc](c => c.quotes.USD.volume_24h);
+    case "priceChangeLast24hUSD":
+      return sort(coins)[sortAsc](c => c.quotes.USD.percent_change_24h);
+    default:
+      return sort(coins)[sortAsc](c => c.quotes.USD.market_cap);
+  }
 }
 
 export default tickerModel;
